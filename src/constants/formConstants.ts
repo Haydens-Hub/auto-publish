661cf20ff97c74989570605bb3b9675dc3bd8140
spencrc <145073726+spencrc@users.noreply.github.com>
2025-09-23 12:44:37 -0400

    formatted formConstants.ts

--- src/constants/formConstants.ts
+++ src/constants/formConstants.ts
@@ -1,16 +1,21 @@
 export const titles = {
-    "name": "Name",
-    "email": "Email",
-    "motivation": "What is your motivation behind writing for Hayden's Journal? Tell us about your life experiences and how your connection to the Hayden's Hub mission.",
-    "ideaDescription": "Tell us about your idea! What kind of article are you thinking of writing about?",
-    "draftFile": "OPTIONAL: Submit your draft (partial, ideas, etc)",
-    "category": "",
-    "missionResonance": "Why does the mission of Hayden's Hub resonate with you? Tell us about yourself and your motivation to share your work. ",
-    "missionRelation": "Briefly explain how your article relates to the Hayden's Hub mission.",
-    "articleFile": "Attach your article draft",
-    "signature": "By signing your name along with the date, you are hereby agreeing to the 'Hayden's Journal- Privacy & Disclosure Agreement'",
-    "questions": "Do you have any questions or concerns"
-}
+  name: "Name",
+  email: "Email",
+  motivation:
+    "What is your motivation behind writing for Hayden's Journal? Tell us about your life experiences and how your connection to the Hayden's Hub mission.",
+  ideaDescription:
+    "Tell us about your idea! What kind of article are you thinking of writing about?",
+  draftFile: "OPTIONAL: Submit your draft (partial, ideas, etc)",
+  category: "",
+  missionResonance:
+    "Why does the mission of Hayden's Hub resonate with you? Tell us about yourself and your motivation to share your work. ",
+  missionRelation:
+    "Briefly explain how your article relates to the Hayden's Hub mission.",
+  articleFile: "Attach your article draft",
+  signature:
+    "By signing your name along with the date, you are hereby agreeing to the 'Hayden's Journal- Privacy & Disclosure Agreement'",
+  questions: "Do you have any questions or concerns?",
+};
 
 export const categories = [
   { value: "academic", label: "Academic Article" },
@@ -21,6 +26,6 @@ export const categories = [
 ];
 
 export const submissionCats = [
-  { value: "pitch", label: "I would like to submit a pitch for an idea"},
-  { value: "draft", label: "I would like to submit my completed draft"}
-]
+  { value: "pitch", label: "I would like to submit a pitch for an idea" },
+  { value: "draft", label: "I would like to submit my completed draft" },
+];
