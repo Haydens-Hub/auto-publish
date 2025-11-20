084a42f35996f3364c3af01d57dcf6ae713bbbd7
spencrc <145073726+spencrc@users.noreply.github.com>
2025-11-19 22:30:21 -0500

    updated post button to reflect changes- now says "create draft" and is blue to differentiate from delete

--- src/components/PublishPostButton.tsx
+++ src/components/PublishPostButton.tsx
@@ -16,9 +16,9 @@ export function PublishPostButton({ id }: { id: string }) {
   return (
     <button
       onClick={removePost}
-      className="rounded-md cursor-pointer bg-[#DF3F29] px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-700"
+      className="rounded-md cursor-pointer bg-[#5D8DCD] px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-blue-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-700"
     >
-      Post
+      Create Draft
     </button>
   );
 }
