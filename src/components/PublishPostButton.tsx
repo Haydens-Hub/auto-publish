5d6abd7da1b24713e3e89c21eea2bddad3e5b83f
spencrc <145073726+spencrc@users.noreply.github.com>
2025-11-22 19:00:20 -0500

    removed unused import; create post button is now disabled while waiting

--- src/components/PublishPostButton.tsx
+++ src/components/PublishPostButton.tsx
@@ -1,24 +1,27 @@
 "use client";
 
-import React from "react";
-import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 export function PublishPostButton({ id }: { id: string }) {
-  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
   const removePost = async () => {
+    setIsLoading(true)
+
     const res = await fetch(`/api/Posts/${id}/publish`, { method: "POST" });
     if (res.ok) {
-      console.log("Post published successfully");
+      console.log("Post successfully created!")
     }
+    setIsLoading(false)
   };
 
   return (
     <button
       onClick={removePost}
-      className="rounded-md cursor-pointer bg-[#5D8DCD] px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-blue-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-700"
+      disabled={isLoading}
+      className="rounded-md cursor-pointer bg-[#5D8DCD] px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-blue-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-700 disabled:opacity-50 disabled:cursor-not-allowed"
     >
-      Create Draft
+      Create Post
     </button>
   );
 }
