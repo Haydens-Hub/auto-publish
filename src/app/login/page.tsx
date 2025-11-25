58ddd27cedcf762a3e349acacf0978389265a207
Sameer Chowdhury <sameerc4402152@gmail.com>
2025-11-24 22:38:00 -0500

    Rewrote the logIn page to be a client login instead of a server login, implemented callback

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,18 @@
-import { signIn } from "@/auth"
- 
+"use client";
+import { signIn } from "next-auth/react"; // client-side signIn
+
 export default function LogIn() {
   return (
-    <form
-      action={async () => {
-        "use server"
-        await signIn("google", {callbackUrl:"/overview"});
-      }}
-      className="flex justify-center items-center min-h-screen bg-[#5D8DCD]"
-    >
-      <div className="max-w-3xl p-12 mx-12 flex text-lg md:text-lg flex-col rounded-2xl text-[#3F493D] bg-[#EFEBEB] w-full shadow-md shadow-[#E6CFB8] justify-center items-center gap-6">
-        <h1 className="text-4xl font-bold mb-4">Sign In to Haydens Hub</h1>
-        <p className="font-medium text-lg">We use google as our authentication provider.</p>
-        <button className="bg-[#5D8DCD] text-white shadow-md p-2 px-8 font-semibold rounded-xl cursor-pointer" type="submit">Sign In</button>
-      </div>
-      
-    </form>
-  )
-} 
+    <div className="flex justify-center items-center min-h-screen bg-[#5D8DCD]">
+    <div className="max-w-3xl p-12 mx-12 flex text-lg md:text-lg flex-col rounded-2xl text-[#3F493D] bg-[#EFEBEB] w-full shadow-md shadow-[#E6CFB8] justify-center items-center gap-6">
+      <h1 className="text-4xl font-bold mb-4">Sign In to Haydens Hub</h1>
+      <p className="font-medium text-lg">We use google as our authentication provider.</p>
+      <button
+        onClick={() => signIn("google", { callbackUrl: "/overview" })}
+        className="bg-[#5D8DCD] text-white shadow-md p-2 px-8 font-semibold rounded-xl cursor-pointer">
+        Sign In
+      </button>
+    </div>
+    </div>
+  );
+}
